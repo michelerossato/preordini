ff871fff0d006758dade642c1a570df7c2c56c95
michelerossato <michelerossato@libero.it>
2025-12-11 15:17:57 +0100

    Update graphicManager.js

--- preordini_lib/util/graphicManager.js
+++ preordini_lib/util/graphicManager.js
@@ -1,218 +1,164 @@
 function GraphicManager() {
 
-    // ============================================================
-    //  GENERA MENU PRINCIPALE CON CATEGORIE + PIETANZE + LINK DOC
-    // ============================================================
-    this.generateMenu = function(hashmap){
+    // ------------------------------------------------------------
+    // GENERA IL MENU PRINCIPALE
+    // ------------------------------------------------------------
+    this.generateMenu = function (hashmap) {
 
-        function generatePietanza(id, nome, prezzo, quantita){
-            return "<div class='content-pietanza-ordine'>" +
-                "<div class='left nome-pietanza'>" +
-                    nome +
-                "</div>" +
-                "<div class='center prezzo-pietanza-ordine'>" +
-                    setTextPrezzo(prezzo) +
-                "</div>" +
-                "<div class='right'>" +
-                    "<div class='left minus'>" +
-                        "<button class='ui-btn brown-btn minus-btn' id='minus" + id + "'>-</button>" +
-                    "</div>" +
-                    "<div class='center quantita-pietanza-ordine'>" +
-                        "<span id='quantita" + id + "'>" + quantita + "</span>" +
-                    "</div>" +
-                    "<div class='right plus'>" +
-                        "<button class='ui-btn brown-btn plus-btn' id='plus" + id + "'>+</button>" +
-                    "</div>" +
-                "</div>" +
-                "<div class='endBlock'></div>" +
-            "</div>";
-        }
+        let html = `
+            <div>
+                <input type="text" id="nomecliente" placeholder="Inserisci il tuo nome">
+                <input type="text" id="tavolo" placeholder="Numero tavolo">
+                <input type="text" id="coperti" placeholder="Quanti siete?">
+            </div>
+        `;
 
-        // ==============================
-        //  Campi cliente
-        // ==============================
-        var txtLista =
-            "<div>" +
-                "<input type='text' id='nomecliente' placeholder='Inserisci il tuo nome'/>" +
-                "<input type='text' id='tavolo' placeholder='Inserisci il numero del tavolo'/>" +
-                "<input type='text' id='coperti' placeholder='Quanti siete?'/>" +
-            "</div>";
+        // Per ogni categoria
+        elencoPrincipale.forEach(cat => {
 
-        // ==============================
-        //  🔥 Link Documento Google
-        // ==============================
-        txtLista +=
-            "<div style='margin: 15px 0; text-align:center;'>" +
-                "<a href='https://docs.google.com/spreadsheets/d/e/2PACX-1vRIyRtTRCMqUH_qI4knGCE-llpqNvfKXW9xEFpa6R4unSNXqlt0zbEThuvy6ugnGTgZl_BNX067D9uy/pub?gid=0&single=true&output=csv'" +
-                " target='_blank' class='ui-btn ui-btn-b ui-corner-all ui-shadow'>" +
-                "📄 Apri Documento Google" +
-                "</a>" +
-            "</div>";
+            const articoli = elencoPietanze[cat];
 
-        // ==============================
-        //  MENU CATEGORIE DAL CSV
-        // ==============================
-        for (var i = 0; i < elencoPrincipale.length; i++) {
+            html += `
+                <div data-role="collapsible">
+                    <h4>${cat}</h4>
+            `;
 
-            var categoriaNome = elencoPrincipale[i];
-            var pietanze = elencoPietanze[categoriaNome];
+            articoli.forEach(a => {
 
-            txtLista += "<div data-role='collapsible'>" +
-                        "<h4>" + categoriaNome + "</h4>";
+                const quantita = hashmap.contains(a.id) ? hashmap.get(a.id) : 0;
 
-            for (var j = 0; j < pietanze.length; j++) {
+                html += `
+                    <div class="content-pietanza-ordine">
+                        <div class="left nome-pietanza">${a.descrizione}</div>
+                        <div class="center prezzo-pietanza-ordine">${a.prezzo.toFixed(2)} €</div>
 
-                var p = pietanze[j];
-                p.nome = p.descrizione;
+                        <div class="right">
+                            <button class="ui-btn brown-btn minus-btn" id="minus${a.id}">-</button>
 
-                var quantita = hashmap.contains(p.id)
-                    ? hashmap.get(p.id)
-                    : 0;
+                            <span id="quantita${a.id}" class="quantita-span">${quantita}</span>
 
-                txtLista += generatePietanza(p.id, p.nome, p.prezzo, quantita);
-            }
-
-            txtLista += "</div>";
-        }
-
-        return txtLista;
-    };
-
-    // ============================================================
-    //  GESTIONE PULSANTI + / -
-    // ============================================================
-    this.setButtonPlusMinus = function(hashmap){
-        var dataManager = new Data();
-
-        for (var i = 0; i < elencoPrincipale.length; i++) {
-            var pietanze = elencoPietanze[elencoPrincipale[i]];
-
-            for (var j = 0; j < pietanze.length; j++) {
-
-                var p = pietanze[j];
-
-                $("#plus" + p.id).off().click(function(){
-                    var id = this.id.substring("plus".length);
-                    var qHtml = $("#quantita" + id);
-                    var q = parseInt(qHtml.html()) + 1;
-                    qHtml.html(q);
-
-                    var hm = dataManager.getInstanceHashmap();
-                    hm.put(id, q);
-                    dataManager.saveInstanceHashmap(hm);
-                });
-
-                $("#minus" + p.id).off().click(function(){
-                    var id = this.id.substring("minus".length);
-                    var qHtml = $("#quantita" + id);
-                    var q = Math.max(parseInt(qHtml.html()) - 1, 0);
-                    qHtml.html(q);
-
-                    var hm = dataManager.getInstanceHashmap();
-                    if(q > 0) hm.put(id, q);
-                    else hm.remove(id);
-
-                    dataManager.saveInstanceHashmap(hm);
-                });
-
-            }
-        }
-    };
-
-    // ============================================================
-    //  POPUP
-    // ============================================================
-    this.generatePopup = function(id, dataElimina){
-
-        var title, info;
-
-        if(dataElimina.value){
-            title = "Elimina Ordine";
-            if(dataElimina.state == 0){
-                info = "<p>Nessuna pietanza selezionata.</p><p>Seleziona almeno una pietanza.</p>";
-            } else {
-                info = "<p>Ordine eliminato.</p><p>Tutte le quantità sono state azzerate.</p>";
-            }
-        } else {
-            title = "Ordine Vuoto";
-            info = "<p>Il tuo ordine è vuoto.</p><p>Seleziona almeno una pietanza.</p>" +
-                   "<button class='ui-btn green-btn' id='ordine-vuoto-btn'>Ok</button>";
-        }
-
-        $("#title-popup").html(title);
-        $("#info-ordine-popup").html(info);
-
-        if(!dataElimina.value){
-            $("#ordine-vuoto-btn").click(function(){
-                $(id).popup("close");
+                            <button class="ui-btn brown-btn plus-btn" id="plus${a.id}">+</button>
+                        </div>
+                        <div class="endBlock"></div>
+                    </div>
+                `;
             });
-        }
+
+            html += `</div>`;
+        });
+
+        return html;
     };
 
-    // ============================================================
-    //  RESOCONTO
-    // ============================================================
-    this.popolaResoconto = function(){
 
-        var hashmap = dataManager.getInstanceHashmap();
-        var keys = hashmap.keys();
-        var totale = 0;
-        var html = "";
+    // ------------------------------------------------------------
+    // GESTIONE BOTTONI + e -
+    // ------------------------------------------------------------
+    this.setButtonPlusMinus = function (hashmap) {
 
-        function getPietanzaById(id){
-            for (var i = 0; i < elencoPrincipale.length; i++){
-                var arr = elencoPietanze[elencoPrincipale[i]];
-                for (var j = 0; j < arr.length; j++)
-                    if(arr[j].id == id) return arr[j];
-            }
-            return null;
-        }
+        const dataManager = new Data();
 
-        for(var i = 0; i < keys.length; i++){
+        elencoPrincipale.forEach(cat => {
 
-            var id = keys[i];
-            var quantita = hashmap.get(id);
-            var p = getPietanzaById(id);
+            elencoPietanze[cat].forEach(p => {
 
-            if(p){
-                var subtot = p.prezzo * quantita;
+                // Bottone +
+                $(`#plus${p.id}`).off().on("click", function () {
+                    let q = parseInt($(`#quantita${p.id}`).text()) + 1;
+                    $(`#quantita${p.id}`).text(q);
 
-                html += "<div class='item-resoconto'>" +
-                        "<span>" + p.descrizione + "</span> x " + quantita +
-                        "<span class='prezzo'>" + setTextPrezzo(subtot) + "</span>" +
-                        "</div>";
+                    hashmap.put(p.id, q);
+                    dataManager.saveInstanceHashmap(hashmap);
+                });
 
-                totale += subtot;
-            }
-        }
+                // Bottone -
+                $(`#minus${p.id}`).off().on("click", function () {
+                    let q = Math.max(parseInt($(`#quantita${p.id}`).text()) - 1, 0);
+                    $(`#quantita${p.id}`).text(q);
 
-        $("#resoconto-lista").html(html);
-        $("#resoconto-totale").html(setTextPrezzo(totale));
+                    if (q === 0) hashmap.remove(p.id);
+                    else hashmap.put(p.id, q);
+
+                    dataManager.saveInstanceHashmap(hashmap);
+                });
+
+            });
+
+        });
+
     };
 
-    // ============================================================
-    //  QR CODE
-    // ============================================================
-    this.popolaQRCode = function(){
 
-        var hashmap = dataManager.getInstanceHashmap();
-        var datiMin = hashmap.toArray();
-        var json = JSON.stringify(datiMin);
+    // ------------------------------------------------------------
+    // POPOLA IL RESOCONTO
+    // ------------------------------------------------------------
+    this.popolaResoconto = function () {
+        let html = "";
+        let hashmap = dataManager.getInstanceHashmap();
 
-        $("#qrcode").html("");
+        const dati = hashmap.toArray();
+        let totale = 0;
+        let quantTot = 0;
+
+        elencoPrincipale.forEach(cat => {
+
+            const articoli = elencoPietanze[cat].filter(x => hashmap.contains(x.id));
+
+            if (articoli.length === 0) return;
+
+            html += `<h3>${cat}</h3>`;
+
+            articoli.forEach(p => {
+
+                const q = hashmap.get(p.id);
+                const prezzoTot = q * p.prezzo;
+
+                totale += prezzoTot;
+                quantTot += q;
+
+                html += `
+                    <div class="riga-resoconto">
+                        <div>${p.descrizione}</div>
+                        <div>x${q}</div>
+                        <div>${prezzoTot.toFixed(2)} €</div>
+                    </div>
+                `;
+            });
+
+        });
+
+        html += `
+            <hr>
+            <h3>Totale: ${totale.toFixed(2)} €</h3>
+        `;
+
+        $("#resoconto").html(html);
+    };
+
+
+    // ------------------------------------------------------------
+    // QR CODE
+    // ------------------------------------------------------------
+    this.popolaQRCode = function () {
+        const hash = dataManager.getInstanceHashmap().toObject();
+        $("#qrcode").empty();
 
         new QRCode(document.getElementById("qrcode"), {
-            text: json,
+            text: JSON.stringify(hash),
             width: 256,
             height: 256
         });
     };
 
-    // ============================================================
-    //  FORMATAZIONE PREZZO
-    // ============================================================
-    function setTextPrezzo(prezzo){
-        if(prezzo % 1 === 0) return prezzo + ",00 Euro";
-        return prezzo.toFixed(2).replace(".", ",") + " Euro";
-    }
+
+    // ------------------------------------------------------------
+    // POPUP
+    // ------------------------------------------------------------
+    this.generatePopup = function (id, dataElimina) {
+        $("#info-ordine-popup").html(`
+            <p>Il tuo ordine è vuoto.</p>
+            <button class="ui-btn green-btn">OK</button>
+        `);
+    };
+
 }
