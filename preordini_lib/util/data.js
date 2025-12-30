19b24c106aec79b943fb708a08ae8c6658db9cd1
michelerossato <michelerossato@libero.it>
2025-12-30 09:42:23 +0100

    Update data.js

--- preordini_lib/util/data.js
+++ preordini_lib/util/data.js
@@ -3,45 +3,51 @@ var elencoPietanze = {};
 
 function DataManager() {
     this.caricaDati = function() {
-        var self = this;
-        var urlWebApp = "https://script.google.com/macros/s/AKfycbxxc7r_TmQwX37jNrp34oB85JjeUNWUj74lvLUfXFRhpeIY8hG5RxaZe8opLZJJ6HU_wQ/exec";
+        var urlWebApp = "https://script.google.com/macros/s/AKfycbxxc7r_TmQwX37jNrp34oB85JjeUNWUj74lvLUfXFRhpeIY8hG5RxaZe8opLZJJ6HU_wQ/exec?callback=?";
 
-        console.log("Tentativo di caricamento dati dalla Web App...");
+        console.log("Caricamento dati tramite JSONP...");
 
-        $.getJSON(urlWebApp, function(data) {
-            // Supponendo che lo script restituisca un array di oggetti
-            data.forEach(function(riga) {
-                var categoria = riga.categoria || riga.Categoria; // Gestisce maiuscole/minuscole
-                var pietanza = {
-                    ID: riga.ID || riga.id,
-                    descrizione: riga.descrizione || riga.Descrizione,
-                    prezzo: riga.prezzo || riga.Prezzo
-                };
+        $.ajax({
+            url: urlWebApp,
+            dataType: 'jsonp', // Indica a jQuery di aspettarsi il formato 'callback(...)'
+            success: function(data) {
+                // Pulizia liste per evitare duplicati in caso di refresh
+                elencoPrincipale = [];
+                elencoPietanze = {};
 
-                if (!elencoPietanze[categoria]) {
-                    elencoPietanze[categoria] = [];
-                    elencoPrincipale.push(categoria);
-                }
-                elencoPietanze[categoria].push(pietanza);
-            });
+                data.forEach(function(riga) {
+                    var categoria = riga.CAT; // Usa 'CAT' come nel tuo file
+                    var pietanza = {
+                        ID: riga.id,          // Usa 'id' come nel tuo file
+                        descrizione: riga.descrizione,
+                        prezzo: riga.prezzo
+                    };
 
-            console.log("Dati caricati con successo dalla Web App.");
-            // Notifica al main.js che può costruire l'interfaccia
-            $(document).trigger("datiPronti");
-        }).fail(function() {
-            console.error("Errore nel caricamento dei dati dalla Web App. Verifica l'URL o i permessi dello script.");
+                    if (!elencoPietanze[categoria]) {
+                        elencoPietanze[categoria] = [];
+                        elencoPrincipale.push(categoria);
+                    }
+                    elencoPietanze[categoria].push(pietanza);
+                });
+
+                console.log("Dati caricati correttamente. Categorie trovate:", elencoPrincipale);
+                $(document).trigger("datiPronti");
+            },
+            error: function(e) {
+                console.error("Errore nel caricamento JSONP:", e);
+                alert("Impossibile caricare il menu. Controlla la connessione.");
+            }
         });
     };
 
-    this.saveInstanceHashmap = function (hashmap) {
+    // Mantieni le funzioni saveInstanceHashmap, getInstanceHashmap e eliminaOrdine identiche a prima
+    this.saveInstanceHashmap = function(hashmap) {
         var str = "";
-        hashmap.keys().forEach(function(key) {
-            str += key + ":" + hashmap.get(key) + ";";
-        });
+        hashmap.keys().forEach(function(key) { str += key + ":" + hashmap.get(key) + ";"; });
         $.cookie("ordine_hashmap", str, { expires: 1 });
     };
 
-    this.getInstanceHashmap = function () {
+    this.getInstanceHashmap = function() {
         var hashmap = new HashMap();
         var cookie = $.cookie("ordine_hashmap");
         if (cookie) {
