a0b88867d126809b884647038d643e8b606a79b7
michelerossato <michelerossato@libero.it>
2025-12-30 09:36:58 +0100

    Update data.js

--- preordini_lib/util/data.js
+++ preordini_lib/util/data.js
@@ -4,18 +4,36 @@ var elencoPietanze = {};
 function DataManager() {
     this.caricaDati = function() {
         var self = this;
-        // Sostituisci questo URL con il link al tuo file CSV/API
-        $.get("preordini_lib/util/menu.csv", function(data) {
-            // Logica di parsing del CSV (semplificata)
-            // Assicurati che qui vengano popolati elencoPrincipale e elencoPietanze
-            console.log("Dati API caricati");
-            
-            // Una volta finito il caricamento, avvisa il sistema
+        var urlWebApp = "https://script.google.com/macros/s/AKfycbxxc7r_TmQwX37jNrp34oB85JjeUNWUj74lvLUfXFRhpeIY8hG5RxaZe8opLZJJ6HU_wQ/exec";
+
+        console.log("Tentativo di caricamento dati dalla Web App...");
+
+        $.getJSON(urlWebApp, function(data) {
+            // Supponendo che lo script restituisca un array di oggetti
+            data.forEach(function(riga) {
+                var categoria = riga.categoria || riga.Categoria; // Gestisce maiuscole/minuscole
+                var pietanza = {
+                    ID: riga.ID || riga.id,
+                    descrizione: riga.descrizione || riga.Descrizione,
+                    prezzo: riga.prezzo || riga.Prezzo
+                };
+
+                if (!elencoPietanze[categoria]) {
+                    elencoPietanze[categoria] = [];
+                    elencoPrincipale.push(categoria);
+                }
+                elencoPietanze[categoria].push(pietanza);
+            });
+
+            console.log("Dati caricati con successo dalla Web App.");
+            // Notifica al main.js che può costruire l'interfaccia
             $(document).trigger("datiPronti");
+        }).fail(function() {
+            console.error("Errore nel caricamento dei dati dalla Web App. Verifica l'URL o i permessi dello script.");
         });
     };
 
-    this.saveInstanceHashmap = function(hashmap) {
+    this.saveInstanceHashmap = function (hashmap) {
         var str = "";
         hashmap.keys().forEach(function(key) {
             str += key + ":" + hashmap.get(key) + ";";
@@ -23,7 +41,7 @@ function DataManager() {
         $.cookie("ordine_hashmap", str, { expires: 1 });
     };
 
-    this.getInstanceHashmap = function() {
+    this.getInstanceHashmap = function () {
         var hashmap = new HashMap();
         var cookie = $.cookie("ordine_hashmap");
         if (cookie) {
@@ -44,5 +62,4 @@ function DataManager() {
 }
 
 var dataManager = new DataManager();
-// Avvia il caricamento dei dati
 dataManager.caricaDati();
